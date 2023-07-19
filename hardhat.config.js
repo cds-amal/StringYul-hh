64bda4187893bf5a2349e3c3340e6b812e27a7a4
cds-amal <cds.sudama@gmail.com>
2023-07-18 20:24:17 -0400

    Add and configure updated/forked hardhat-yul plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@tovarishfin/hardhat-yul");
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const YulString = require("./yulstring.config");
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  yulArtifacts: { YulString },
 };
---
+++ yulstring.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  abi: [
+    {
+      "inputs": [],
+      "name": "getValue",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string",
+        },
+      ],
+      "stateMutability": "view",
+      "type": "function",
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "input",
+          "type": "string",
+        },
+      ],
+      "name": "setValue",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function",
+    },
+  ],
+};
